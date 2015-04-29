4c806f5b6f90e8f189b020f2d6960b8f34ce2df4
Balazs Kegl <balazs.kegl@gmail.com>
2015-04-29 22:35:13 +0200

    t1

--- calibrator.py
+++ calibrator.py
@@ -1,31 +1,56 @@
 import numpy as np
-from sklearn.isotonic import IsotonicRegression
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.base import BaseEstimator
+import os
+os.environ["OMP_NUM_THREADS"] = "1"
+import calibration_plots
 
-class Calibrator():
+import theano
+from lasagne.layers import DenseLayer
+from lasagne.layers import InputLayer
+from lasagne.layers import DropoutLayer
+from lasagne.nonlinearities import softmax
+from lasagne.updates import nesterov_momentum
+from nolearn.lasagne import NeuralNet
+
+class Calibrator(BaseEstimator):
     def __init__(self):
-        pass
+        self.net = None
+        self.label_encoder = None
     
     def fit(self, X_array, y_pred_array):
         labels = np.sort(np.unique(y_pred_array))
         num_classes = X_array.shape[1]
-        self.calibrators = []
-        for class_index in range(num_classes):
-            calibrator = IsotonicRegression(
-                y_min=0., y_max=1., out_of_bounds='clip')
-            class_indicator = np.array([1 if y == labels[class_index] else 0 
-                                        for y in y_pred_array])
-            calibrator.fit(np.nan_to_num(X_array[:,class_index]), class_indicator)
-            self.calibrators.append(calibrator)
-          
+        layers0 = [('input', InputLayer),
+                   ('dense', DenseLayer),
+                   ('output', DenseLayer)]
+        X = X_array.astype(theano.config.floatX)
+        self.scaler = StandardScaler()
+        X = self.scaler.fit_transform(X)
+        self.label_encoder = LabelEncoder()
+        #y = class_indicators.astype(np.int32)
+        y = self.label_encoder.fit_transform(y_pred_array).astype(np.int32)
+        scaler = StandardScaler()
+        X = scaler.fit_transform(X)
+        num_features = X.shape[1]
+        self.net = NeuralNet(layers=layers0,
+                             input_shape=(None, num_features),
+                             dense_num_units=num_features,
+                             output_num_units=num_classes,
+                             output_nonlinearity=softmax,
+
+                             update=nesterov_momentum,
+                             update_learning_rate=0.01,
+                             update_momentum=0.9,
+
+                             eval_size=0.2,
+                             verbose=1,
+                             max_epochs=100)
+        self.net.fit(X, y)
+
     def predict_proba(self, y_probas_array_uncalibrated):
         num_classes = y_probas_array_uncalibrated.shape[1]
-        y_probas_array_transpose = np.array(
-            [self.calibrators[class_index].predict(
-                np.nan_to_num(y_probas_array_uncalibrated[:,class_index]))
-             for class_index in range(num_classes)])
-
-        sum_rows = np.sum(y_probas_array_transpose, axis=0)
-        y_probas_array_normalized_transpose = np.divide(
-            y_probas_array_transpose, sum_rows)
-
-        return y_probas_array_normalized_transpose.T
+        X = y_probas_array_uncalibrated.astype(theano.config.floatX)
+        X = self.scaler.fit_transform(X)
+        return self.net.predict_proba(X)
--- classifier.py
+++ classifier.py
@@ -1,61 +1,23 @@
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.preprocessing import Imputer
+from sklearn.pipeline import Pipeline
 from sklearn.base import BaseEstimator
-
-import numpy as np
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import StandardScaler
-import os
-os.environ["OMP_NUM_THREADS"] = "1"
-
-import theano
-from lasagne.layers import DenseLayer
-from lasagne.layers import InputLayer
-from lasagne.layers import DropoutLayer
-from lasagne.nonlinearities import softmax
-from lasagne.updates import nesterov_momentum
-from nolearn.lasagne import NeuralNet
-
-
+ 
 class Classifier(BaseEstimator):
-
     def __init__(self):
-        self.net = None
-        self.label_encoder = None
-
+        self.clf = Pipeline([
+            ('imputer', Imputer(strategy='most_frequent')),
+            ('rf', AdaBoostClassifier(
+                base_estimator=RandomForestClassifier(max_depth=5, n_estimators=10),
+                n_estimators=10)
+            )
+        ])
+ 
     def fit(self, X, y):
-        layers0 = [('input', InputLayer),
-                   ('dense0', DenseLayer),
-                   ('dropout', DropoutLayer),
-                   ('dense1', DenseLayer),
-                   ('output', DenseLayer)]
-        X = X.astype(theano.config.floatX)
-        self.label_encoder = LabelEncoder()
-        y = self.label_encoder.fit_transform(y).astype(np.int32)
-        scaler = StandardScaler()
-        X = scaler.fit_transform(X)
-        num_classes = len(self.label_encoder.classes_)
-        num_features = X.shape[1]
-        self.net = NeuralNet(layers=layers0,
-                             input_shape=(None, num_features),
-                             dense0_num_units=500,
-                             dropout_p=0.5,
-                             dense1_num_units=200,
-                             output_num_units=num_classes,
-                             output_nonlinearity=softmax,
-
-                             update=nesterov_momentum,
-                             update_learning_rate=0.05,
-                             update_momentum=0.9,
-
-                             eval_size=0.2,
-                             verbose=1,
-                             max_epochs=20)
-        self.net.fit(X, y)
-        return self
-
+        self.clf.fit(X, y)
+ 
     def predict(self, X):
-        X = X.astype(theano.config.floatX)
-        return self.label_encoder.inverse_transform(self.net.predict(X))
-
+        return self.clf.predict(X)
+ 
     def predict_proba(self, X):
-        X = X.astype(theano.config.floatX)
-        return self.net.predict_proba(X)
+        return self.clf.predict_proba(X)
