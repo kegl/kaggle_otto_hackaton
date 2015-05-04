5b0a0549e8ef1c4680d71da9b6e875ca016f9de8
Balazs Kegl <balazs.kegl@gmail.com>
2015-05-04 10:52:29 +0200

    an1

--- classifier.py
+++ classifier.py
@@ -1,38 +1,108 @@
-from sklearn.preprocessing import StandardScaler
-from sklearn.base import BaseEstimator
-from sklearn.preprocessing import LabelEncoder
 import numpy as np
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.preprocessing import Imputer, LabelEncoder, StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.base import BaseEstimator
+ 
+import os
+os.environ["OMP_NUM_THREADS"] = "1"
+ 
+import theano
+from lasagne.layers import DenseLayer
+from lasagne.layers import InputLayer
+from lasagne.layers import DropoutLayer
+from lasagne.nonlinearities import softmax
+from lasagne.nonlinearities import rectify
+from lasagne.updates import nesterov_momentum
+from lasagne.updates import adagrad
+from nolearn.lasagne import NeuralNet
+ 
+ 
+class BaseClassifier(BaseEstimator):
+ 
+    def __init__(self):
+        self.net = None
+        self.label_encoder = None
+ 
+    def fit(self, X, y):
+        layers0 = [('input', InputLayer),
+                   ('dense0', DenseLayer),
+                   ('dropout', DropoutLayer),
+                   ('dense1', DenseLayer),
+                   ('output', DenseLayer)]
+        X = X.astype(theano.config.floatX)
+        self.label_encoder = LabelEncoder()
+        y = self.label_encoder.fit_transform(y).astype(np.int32)
+        self.scaler = StandardScaler()
+        X = self.scaler.fit_transform(X)
+        num_classes = len(self.label_encoder.classes_)
+        num_features = X.shape[1]
+        self.net = NeuralNet(layers=layers0,
+                             input_shape=(None, num_features),
+                             dense0_num_units=200,
+                             dense0_nonlinearity=rectify,
+                             dropout_p=0.5,
+                             dense1_num_units=200,
+                             dense1_nonlinearity=rectify,
+                             output_num_units=num_classes,
+                             output_nonlinearity=softmax,
+ 
+                             update=adagrad,
+                             update_learning_rate=0.02,
+ 
+                             eval_size=0.2,
+                             verbose=1,
+                             max_epochs=20,
+                             )
+        self.net.fit(X, y)
+        return self
+ 
+    def predict(self, X):
+        X = X.astype(theano.config.floatX)
+        X = self.scaler.fit_transform(X)
+        return self.label_encoder.inverse_transform(self.net.predict(X))
+ 
+    def predict_proba(self, X):
+        X = X.astype(theano.config.floatX)
+        X = self.scaler.fit_transform(X)
+        return self.net.predict_proba(X)
 
-#import sys
-#sys.path.append('C:\\src\c++\\xgboost\\wrapper')
-import xgboost as xgb
+class WeightedBaseClassifier(BaseEstimator):
+    def __init__(self, base_classifier):
+        self.base_classifier = base_classifier
+ 
+    def fit(self, X, y, sample_weight, n_sample = 0):
+        self.class_label_encoder_ = LabelEncoder()
+        self.class_label_encoder_.fit(y)
+        self.classes_ = self.class_label_encoder_.classes_
+        if n_sample == 0:
+            n_sample = X.shape[0]
+        sample_indexes = np.random.choice(
+            range(X.shape[0]), size=n_sample, p=sample_weight)
+        X_sampled = X[sample_indexes]          
+        y_sampled = y[sample_indexes]          
+        self.base_classifier.fit(X, y)  
+ 
+    def predict(self, X):
+        pred = self.base_classifier.predict(X)
+        return self.class_label_encoder_.inverse_transform(pred)  
+
+    def predict_proba(self, X):
+        return self.base_classifier.predict_proba(X)
 
 class Classifier(BaseEstimator):
     def __init__(self):
-        self.label_encoder = LabelEncoder()
-        self.scaler = StandardScaler()
-        self.clf = None
-        self.param = {'eval_metric':'mlogloss'}
-        self.param['num_class'] = 9
-        self.param['subsample'] = 0.9        
-        self.num_round = 100
-        self.obj = 'multi:softprob'
+        self.clf = AdaBoostClassifier(
+            base_estimator=WeightedBaseClassifier(BaseClassifier()),
+            n_estimators=10
+        )
  
-    def fit(self, X, y):        
-        X = self.scaler.fit_transform(X.astype(np.float32))              
-        y = self.label_encoder.fit_transform(y).astype(np.int32)
-        dtrain = xgb.DMatrix( X, label=y.astype(np.float32))
-        
-        self.param['objective'] = self.obj  
-        self.clf = xgb.train(self.param, dtrain, self.num_round)  
+    def fit(self, X, y):
+        self.clf.fit(X, y)
  
     def predict(self, X):
-        X = self.scaler.transform(X.astype(np.float32))
-        dtest = xgb.DMatrix(X)       
-        label_index_array = np.argmax(self.clf.predict(dtest), axis=1)
-        return self.label_encoder.inverse_transform(label_index_array)
+        return self.clf.predict(X)
  
     def predict_proba(self, X):
-        X = self.scaler.transform(X.astype(np.float32))
-        dtest = xgb.DMatrix(X)
-        return self.clf.predict(dtest)
+        return self.clf.predict_proba(X)
+
