7289a4ebf9a6de3b967886634fd252191d0f8276
Balazs Kegl <balazs.kegl@gmail.com>
2015-05-05 15:16:31 +0200

    an3

--- classifier.py
+++ classifier.py
@@ -6,46 +6,67 @@ from sklearn.base import BaseEstimator
  
 import os
 os.environ["OMP_NUM_THREADS"] = "1"
-
-from lasagne.easy import SimpleNeuralNet
+ 
 import theano
-
+from lasagne.layers import DenseLayer
+from lasagne.layers import InputLayer
+from lasagne.layers import DropoutLayer
+from lasagne.nonlinearities import softmax
+from lasagne.nonlinearities import rectify
+from lasagne.updates import nesterov_momentum
+from lasagne.updates import adagrad
+from nolearn.lasagne import NeuralNet
+ 
+ 
 class BaseClassifier(BaseEstimator):
  
     def __init__(self):
- 
-        self.clf = Pipeline([
-            ('log', LogScaler()),
-            ('scaler', StandardScaler()),
-            ('neuralnet', SimpleNeuralNet(nb_hidden_list=[1000],
-                                          max_nb_epochs=30,
-                                          batch_size=256,
-                                          learning_rate=1.,
-                                          L1_factor=0.0001)),
-        ])
+        self.net = None
+        self.label_encoder = None
  
     def fit(self, X, y):
+        layers0 = [('input', InputLayer),
+                   ('dense0', DenseLayer),
+                   ('dropout', DropoutLayer),
+                   ('dense1', DenseLayer),
+                   ('output', DenseLayer)]
         X = X.astype(theano.config.floatX)
-        self.clf.fit(X, y)
+        self.label_encoder = LabelEncoder()
+        y = self.label_encoder.fit_transform(y).astype(np.int32)
+        self.scaler = StandardScaler()
+        X = self.scaler.fit_transform(X)
+        num_classes = len(self.label_encoder.classes_)
+        num_features = X.shape[1]
+        self.net = NeuralNet(layers=layers0,
+                             input_shape=(None, num_features),
+                             dense0_num_units=300,
+                             dense0_nonlinearity=rectify,
+                             dropout_p=0.5,
+                             dense1_num_units=200,
+                             dense1_nonlinearity=rectify,
+                             output_num_units=num_classes,
+                             output_nonlinearity=softmax,
+ 
+                             update=adagrad,
+                             update_learning_rate=0.02,
+ 
+                             eval_size=0.2,
+                             verbose=1,
+                             max_epochs=20,
+                             )
+        self.net.fit(X, y)
         return self
  
     def predict(self, X):
         X = X.astype(theano.config.floatX)
-        return self.clf.predict(X)
+        X = self.scaler.fit_transform(X)
+        return self.label_encoder.inverse_transform(self.net.predict(X))
  
     def predict_proba(self, X):
         X = X.astype(theano.config.floatX)
-        return self.clf.predict_proba(X)
+        X = self.scaler.fit_transform(X)
+        return self.net.predict_proba(X)
  
-class LogScaler(object):
- 
-    def fit(self, X, y=None):
-        return self
- 
-    def transform(self, X):
-        return np.log(1 + X)
- 
-
 class WeightedBaseClassifier(BaseEstimator):
     def __init__(self, base_classifier):
         self.base_classifier = base_classifier
@@ -60,20 +81,20 @@ class WeightedBaseClassifier(BaseEstimator):
             range(X.shape[0]), size=n_sample, p=sample_weight)
         X_sampled = X[sample_indexes]          
         y_sampled = y[sample_indexes]          
-        self.base_classifier.fit(X, y)  
+        self.base_classifier.fit(X_sampled, y_sampled)  
  
     def predict(self, X):
         pred = self.base_classifier.predict(X)
         return self.class_label_encoder_.inverse_transform(pred)  
-
+ 
     def predict_proba(self, X):
         return self.base_classifier.predict_proba(X)
-
+ 
 class Classifier(BaseEstimator):
     def __init__(self):
         self.clf = AdaBoostClassifier(
             base_estimator=WeightedBaseClassifier(BaseClassifier()),
-            n_estimators=3
+            n_estimators=10
         )
  
     def fit(self, X, y):
@@ -84,4 +105,3 @@ class Classifier(BaseEstimator):
  
     def predict_proba(self, X):
         return self.clf.predict_proba(X)
-
